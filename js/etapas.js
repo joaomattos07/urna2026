f801ba0f3ff16185c99655b4a37ce94e4bc589b3
João Pedro <joaopedro261206@gmail.com>
2026-08-31 22:21:17 -0300

    Imagens atualizadas

--- js/etapas.js
+++ js/etapas.js
@@ -3,28 +3,28 @@ let etapas = [
         titulo: 'VEREADOR',
         numeros: 5,
         candidatos: [
-        { numero: '00001', nome: 'José Silva', partido: 'Brasil', fotos: [{ url: '.jpg', legenda: 'Vereador' }] },
-        { numero: '00002', nome: 'Marina Santos', partido: 'Brasil', fotos: [{ url: '.jpg', legenda: 'Vereador' }] },
-        { numero: '00003', nome: 'Willian Gomes', partido: 'Brasil', fotos: [ {url:'.jpg', legenda: 'Vereador'} ] },
-        { numero: '00004', nome: 'Amanda Costa', partido: 'Brasil', fotos: [ {url:'.jpg', legenda: 'Vereador'} ]},
+        { numero: '00001', nome: 'José Silva', partido: 'Brasil', fotos: [{ url: 'jose.jpg', legenda: 'Vereador' }] },
+        { numero: '00002', nome: 'Marina Santos', partido: 'Brasil', fotos: [{ url: 'marina.jpg', legenda: 'Vereador' }] },
+        { numero: '00003', nome: 'Willian Gomes', partido: 'Brasil', fotos: [ {url:'willian.jpg', legenda: 'Vereador'} ] },
+        { numero: '00004', nome: 'Amanda Costa', partido: 'Brasil', fotos: [ {url:'amanda.jpg', legenda: 'Vereador'} ]},
         ]
     },
     {
         titulo: 'PREFEITO',
         numeros: 2,
         candidatos: [
-            { numero: '01', nome: 'Mariana Rodrigues', partido: 'Brasil', fotos: [{ url: '.jpg', legenda: 'Prefeito' },] },
-            { numero: '02', nome: 'João Almeida', partido: 'Brasil', fotos: [{ url: '.jpg', legenda: 'Prefeito' }] },
-            { numero: '03', nome: 'Pedro Afonso', partido: 'Brasil', fotos: [{ url: '.jpg', legenda: 'Prefeito' }] }
+            { numero: '01', nome: 'Mariana Rodrigues', partido: 'Brasil', fotos: [{ url: 'mariana.jpg', legenda: 'Prefeito' },] },
+            { numero: '02', nome: 'João Almeida', partido: 'Brasil', fotos: [{ url: 'joao.jpg', legenda: 'Prefeito' }] },
+            { numero: '03', nome: 'Pedro Afonso', partido: 'Brasil', fotos: [{ url: 'pedro.jpg', legenda: 'Prefeito' }] }
         ]
     },
     {
         titulo: 'PRESIDENTE',
         numeros: 2,
         candidatos: [
-            { numero: '01', nome: 'Geovana Gonçalves', partido: 'Brasil', fotos: [{ url: '.jpg', legenda: 'Presidente' }] },
-            { numero: '02', nome: 'Lucas Oliveira', partido: 'Brasil', fotos: [{ url: '.jpg', legenda: 'Presidente' }] },
-            { numero: '03', nome: 'Matheus Pereira', partido: 'Brasil', fotos: [{ url: '.jpg', legenda: 'Presidente' }] }
+            { numero: '01', nome: 'Geovana Gonçalves', partido: 'Brasil', fotos: [{ url: 'geovana.jpg', legenda: 'Presidente' }] },
+            { numero: '02', nome: 'Lucas Oliveira', partido: 'Brasil', fotos: [{ url: 'lucas.png', legenda: 'Presidente' }] },
+            { numero: '03', nome: 'Matheus Pereira', partido: 'Brasil', fotos: [{ url: 'matheus.jpg', legenda: 'Presidente' }] }
         ]
     }
 ];
